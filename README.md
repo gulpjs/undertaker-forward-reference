a191df7473bc09feb45d7b3125953132af011722
Peter deHaan <peter@deseloper.com>
2015-10-25 17:33:19 -0700

    Build: Add eslint & jscs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # undertaker-forward-reference
 
-[![Travis Build Status](https://img.shields.io/travis/undertakerjs/undertaker-forward-reference/master.svg?label=travis&style=flat-square)](https://travis-ci.org/undertakerjs/undertaker-forward-reference)
+[![Travis Build Status](https://img.shields.io/travis/gulpjs/undertaker-forward-reference/master.svg?label=travis&style=flat-square)](https://travis-ci.org/gulpjs/undertaker-forward-reference)
 
 Undertaker registry supporting forward referenced tasks
 
--- test/index.js
+++ test/index.js
@@ -5,40 +5,40 @@ var code = require('code');
 
 var Registry = require('../');
 
-function noop(){}
+function noop() {}
 
-lab.experiment('Forward Reference Registry', function(){
+lab.experiment('Forward Reference Registry', function() {
 
   var registry;
 
-  lab.beforeEach(function(done){
+  lab.beforeEach(function(done) {
     registry = new Registry();
     done();
   });
 
-  lab.test('returns a function even if not registered', function(done){
+  lab.test('returns a function even if not registered', function(done) {
     var task = registry.get('nothing');
     code.expect(task).to.be.a.function();
     done();
   });
 
-  lab.test('forward reference function mimics task name', function(done){
+  lab.test('forward reference function mimics task name', function(done) {
     var task = registry.get('nothing');
     code.expect(task.displayName).to.equal('nothing');
     done();
   });
 
-  lab.test('returns the task if already registered', function(done){
+  lab.test('returns the task if already registered', function(done) {
     registry.set('nothing', noop);
     var task = registry.get('nothing');
     code.expect(task).to.equal(noop);
     done();
   });
 
-  lab.test('excutes task when forward reference is called', function(done){
+  lab.test('excutes task when forward reference is called', function(done) {
     var count = 0;
 
-    function actualTask(){
+    function actualTask() {
       count++;
       code.expect(count).to.equal(1);
       done();
@@ -50,7 +50,7 @@ lab.experiment('Forward Reference Registry', function(){
     task();
   });
 
-  lab.test('throws if task is not defined before forward ref is called', function(done){
+  lab.test('throws if task is not defined before forward ref is called', function(done) {
     var task = registry.get('nothing');
     code.expect(task).to.throw('Forward referenced task \'nothing\' not defined before use');
     done();
